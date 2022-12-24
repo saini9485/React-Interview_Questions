0ed6764fe96c8698784efe6e02a4ca5b03d04718
Rajesh Saini <saini222302@gmail.com>
2022-12-24 17:03:28 +0530

    InterviewCounter

---
+++ src/components/MapCounter.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+export  function MapCounter() {
+  let [count, setCount] = useState(0);
+  let [arr, setArr] = useState([]);
+
+  let handleCount = () => {
+    setCount((count)=> count+ 1);
+    setArr([...arr, count]);
+  };
+
+  return (
+    <div>
+      {arr.map((count, index) => index>=1? <span>{count}</span>:"")}
+      <button
+        onClick={() => {
+          handleCount();
+        }}
+      >
+        Increment
+      </button>
+    </div>
+  );
+}
