1d555144be8033fffc0c004e7eca5492bbd4305b
Rajesh Saini <saini222302@gmail.com>
2022-12-28 11:45:59 +0530

    Interview Question

--- src/components/Memoziation.jsx
+++ src/components/Memoziation.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React,{useMemo, useState} from 'react'
+
 
 export  function Memoziation() {
+const [random, setRandom] = useState()
+const addRandom = () => {
+     setRandom([...Math.random().toFixed(2), Math.random().toFixed(2)[Math.random().toFixed(2).length-1]+1])
+
+}
+ 
+    
+    const randomM = useMemo(()=> Math.random().toFixed(2),[])
+   
+    
+
+  
+
   return (
-    <div>Memoziation</div>
+    <div>
+        Memoziation
+        <h1>without:-{random}</h1>
+        <h1>Memoization:-{randomM}</h1>
+        <button onClick={addRandom}>Add</button>
+        </div>
   )
 }
