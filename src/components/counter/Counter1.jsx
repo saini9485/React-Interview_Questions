393fbfd77a592c05ad8fb2b2c33fbca8a87e641f
Rajesh Saini <saini222302@gmail.com>
2023-03-13 04:43:49 +0530

    interview questions

--- src/components/counter/Counter1.jsx
+++ src/components/counter/Counter1.jsx
@@ -1,8 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 export function Counter1 (){
+    const[counter, setCounter] = useState(0)
+
+const handleIncreament = () =>{
+    setCounter(counter+1)
+}
+const handleDecreament = () =>{
+    setCounter(counter-1)
+}
     return (
         <div>
+            <h1>{counter}</h1>
             <h1>Counter</h1>
+            <button onClick={handleIncreament}>Increament</button>
+            <button onClick={handleDecreament} disabled = {counter==0} >Decreament</button>
         </div>
     )
 }
